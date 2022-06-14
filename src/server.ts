d4ca308c338b7f0b2c2122a9e88ecaa0009c5128
Danilo <danilosdinizz@gmail.com>
2022-06-14 00:00:26 -0300

    <feat>: starting the server development

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3333, ()=> console.log('server is running'));
+
+
+app.get('/', (request, response) =>{
+    response.json({msg:'hello world'})
+});
