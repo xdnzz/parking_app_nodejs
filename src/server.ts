0eca2c94c081c4defcab4fb9061b1d5799056744
Danilo <danilosdinizz@gmail.com>
2022-06-14 21:16:06 -0300

    feat: categories route added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import {categoriesRoutes} from '../src/routes/categories.routes'
+
+
 
 const app = express();
 
+app.use(express.json());  
+
+app.use(categoriesRoutes);
+
 app.listen(3333, ()=> console.log('server is running'));
 
-
-app.get('/', (request, response) =>{
-    response.json({msg:'hello world'})
-});
